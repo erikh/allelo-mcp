b9875bc0f563045aa38232d4086be6ccddd5eb07
Erik Hollensbe <git@hollensbe.org>
2025-08-09 13:27:10 -0700

    PoC working SSE client. does nothing yet
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/api/client/mod.rs
+++ src/api/client/mod.rs
@@ -1,19 +1,21 @@
 use super::server::Prompt;
-use anyhow::Result;
 use futures_util::StreamExt;
 use reqwest_eventsource::Event;
+use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver};
 
 #[derive(Debug, Clone)]
 pub struct Client {
     base_url: url::Url,
 }
 
+pub type SseResult = Result<UnboundedReceiver<Result<Event, anyhow::Error>>, anyhow::Error>;
+
 impl Client {
     pub fn new(base_url: url::Url) -> Self {
         Self { base_url }
     }
 
-    pub async fn prompt(&self, input: Prompt) -> Result<()> {
+    pub async fn prompt(&self, input: Prompt) -> SseResult {
         let url = self.base_url.join("/prompt")?;
         let mut es = reqwest_eventsource::EventSource::post(
             url.to_string(),
@@ -21,14 +23,31 @@ impl Client {
             reqwest::Body::from(serde_json::to_vec(&input)?),
         );
 
-        while let Some(event) = es.next().await {
-            match event {
-                Ok(Event::Open) => {}
-                Ok(Event::Message(_m)) => {}
-                Err(e) => return Err(e.into()),
-            }
-        }
+        let (s, r) = unbounded_channel();
 
-        Ok(())
+        tokio::spawn(async move {
+            while let Some(event) = es.next().await {
+                match event {
+                    Ok(m) => match s.send(Ok(m)) {
+                        // try to send an error if we get one trying to send. We probably won't
+                        // succeed, so don't try to handle further errors.
+                        Err(e) => {
+                            let _ = s.send(Err(e.into()));
+                            return;
+                        }
+                        _ => {}
+                    },
+                    Err(e) => {
+                        // if we already have an error, just try to fire off the event before
+                        // dying, don't try to process any trouble with firing it to avoid
+                        // complexity.
+                        let _ = s.send(Err(e.into()));
+                        return;
+                    }
+                }
+            }
+        });
+
+        Ok(r)
     }
 }
--- src/api/server/tests.rs
+++ src/api/server/tests.rs
@@ -1,8 +1,34 @@
 use super::*;
 use crate::testutil::{shutdown_handle, start_api_server};
+use reqwest_eventsource::Event;
 
 #[tokio::test]
-async fn test() {
+async fn test_sse() {
     let handle = start_api_server(Config::default()).await.unwrap();
-    shutdown_handle(handle)
+    let client = super::super::client::Client::new("http://localhost:8999".parse().unwrap());
+    let mut r = client
+        .prompt(Prompt {
+            prompt: "hello, world".into(),
+        })
+        .await
+        .unwrap();
+
+    let mut i = 0;
+
+    while let Some(Ok(m)) = r.recv().await {
+        if i == 10 {
+            break;
+        }
+        match m {
+            Event::Open => {}
+            Event::Message(m) => {
+                i += 1;
+                eprintln!("{}", m.data);
+            }
+        }
+    }
+
+    assert_eq!(i, 10);
+
+    shutdown_handle(handle);
 }
