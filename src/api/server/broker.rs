6c25527bfa9cc1e8b5994051ff273c58ea6b2781
Erik Hollensbe <git@hollensbe.org>
2025-08-10 12:52:01 -0700

    stub out rest of broker methods
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/api/server/broker.rs
+++ src/api/server/broker.rs
@@ -2,8 +2,18 @@
 
 use super::McpRequest;
 use anyhow::Result;
-use std::{collections::HashMap, sync::Arc};
-use tokio::sync::mpsc::{channel, Receiver, Sender};
+use std::{
+    collections::HashMap,
+    sync::{Arc, LazyLock},
+};
+use tokio::sync::{
+    mpsc::{channel, Receiver, Sender},
+    Mutex,
+};
+use uuid::Uuid;
+
+pub type GlobalBroker = Arc<Mutex<Broker>>;
+pub static GLOBAL_BROKER: LazyLock<GlobalBroker> = LazyLock::new(|| Default::default());
 
 const CHANNEL_SIZE: usize = 1000;
 
@@ -34,8 +44,40 @@ where
 // NOTE: this is probably not a long-term solution, but it should route requests between the API
 // service and the various AI services / MCPs involved in the process. It will likely use a lot of
 // memory and will likely need to be replaced with a dedicated queue before using in production.
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default)]
 pub struct Broker {
     mcp: HashMap<uuid::Uuid, Arc<BrokerPipe<McpRequest>>>,
     prompt: HashMap<uuid::Uuid, Arc<BrokerPipe<String>>>,
+}
+
+impl Broker {
+    // FIXME: replace anyhow with thiserror here
+    pub fn create_mcp(&mut self) -> Result<uuid::Uuid> {
+        let uuid = Uuid::new_v4();
+        self.mcp.insert(uuid, Arc::new(BrokerPipe::new()));
+        Ok(uuid)
+    }
+
+    // FIXME: replace anyhow with thiserror here
+    pub fn create_prompt(&mut self) -> Result<uuid::Uuid> {
+        let uuid = Uuid::new_v4();
+        self.prompt.insert(uuid, Arc::new(BrokerPipe::new()));
+        Ok(Default::default())
+    }
+
+    pub fn get_mcp(&self, id: uuid::Uuid) -> Option<Arc<BrokerPipe<McpRequest>>> {
+        self.mcp.get(&id).cloned()
+    }
+
+    pub fn get_prompt(&self, id: uuid::Uuid) -> Option<Arc<BrokerPipe<String>>> {
+        self.prompt.get(&id).cloned()
+    }
+
+    pub fn expire_mcp(&mut self, id: uuid::Uuid) {
+        self.mcp.remove(&id);
+    }
+
+    pub fn expire_prompt(&mut self, id: uuid::Uuid) {
+        self.prompt.remove(&id);
+    }
 }
