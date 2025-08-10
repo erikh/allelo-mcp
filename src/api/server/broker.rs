bf8ae59a21c2be2371ccaf76e9a9d3ab173743c5
Erik Hollensbe <git@hollensbe.org>
2025-08-10 11:27:57 -0700

    almost certainly going to regret using a static lifetime later
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/api/server/broker.rs
+++ src/api/server/broker.rs
@@ -1,10 +1,9 @@
 #![allow(unused)]
 
-use std::{collections::HashMap, sync::Arc};
-
-use tokio::sync::mpsc::{channel, Receiver, Sender};
-
 use super::McpRequest;
+use anyhow::{anyhow, Result};
+use std::{collections::HashMap, sync::Arc};
+use tokio::sync::mpsc::{channel, Receiver, Sender};
 
 const CHANNEL_SIZE: usize = 1000;
 
@@ -14,7 +13,29 @@ pub struct BrokerPipe<T> {
     receiver: Receiver<T>,
 }
 
+impl<T> BrokerPipe<T>
+where
+    T: Sync + Send + 'static,
+{
+    pub fn new() -> Self {
+        let (sender, receiver) = channel(CHANNEL_SIZE);
+        Self { sender, receiver }
+    }
+
+    pub async fn next_message(&mut self) -> Option<T> {
+        self.receiver.recv().await
+    }
+
+    pub async fn send_message(&mut self, msg: T) -> Result<()> {
+        Ok(self.sender.send(msg).await?)
+    }
+}
+
+// NOTE: this is probably not a long-term solution, but it should route requests between the API
+// service and the various AI services / MCPs involved in the process. It will likely use a lot of
+// memory and will likely need to be replaced with a dedicated queue before using in production.
 #[derive(Debug, Clone)]
 pub struct Broker {
-    mcp_routes: HashMap<uuid::Uuid, Arc<BrokerPipe<McpRequest>>>,
+    mcp: HashMap<uuid::Uuid, Arc<BrokerPipe<McpRequest>>>,
+    prompt: HashMap<uuid::Uuid, Arc<BrokerPipe<String>>>,
 }
