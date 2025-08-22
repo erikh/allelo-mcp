a8dcf94f6ed28e6a5e110109ac696d31d70c5703
Erik Hollensbe <git@hollensbe.org>
2025-08-22 12:23:29 -0700

    another important comment
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/api/llm/mod.rs
+++ src/api/llm/mod.rs
@@ -1,10 +1,13 @@
 #![allow(dead_code)]
 
-// BOTE: the underlying LLM client's abstraction is not much different than this one. I chose to
+// NOTE: the underlying LLM client's abstraction is not much different than this one. I chose to
 // NIH this so I'd have control of the inner workings. Don't get mad, modifying it to support new
 // APIs should not be very complicated if the llm crate supports it already.
 
 pub enum LLMClientType {
+    // NOTE: please provide diverse clients for different models, so they can be pre-programmed
+    // with appropriate parameters independently without forcing this to be a part of the client
+    // user's implementation.
     Ollama,
     Claude,
 }
@@ -18,7 +21,6 @@ pub struct LLMClient {
 pub struct LLMClientParams {
     base_url: String,
     api_key: Option<String>,
-    model: String,
     max_tokens: Option<u32>,
     timeout: Option<std::time::Duration>,
     // FIXME: json schema response support
